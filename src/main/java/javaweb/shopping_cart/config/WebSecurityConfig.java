722bb484505813c454964dc0292d4c6d6d113091
VeeJ <vesy17032003@gmail.com>
2024-10-09 16:21:36 +0700

    Dev ve fix

--- src/main/java/javaweb/shopping_cart/config/WebSecurityConfig.java
+++ src/main/java/javaweb/shopping_cart/config/WebSecurityConfig.java
@@ -3,9 +3,9 @@ package javaweb.shopping_cart.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,23 +24,23 @@ public class WebSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .requestMatchers("/home","/login","/403","/css/**","/images/**").permitAll()
+                .requestMatchers("/", "/home", "/login", "/403","/css/**","/images/**").permitAll()
                 .requestMatchers("/registration","/webjars/**").permitAll()
                 .requestMatchers("/products").permitAll()
                 .anyRequest().authenticated()
-        ).formLogin(
-                (formBased) -> {
-                    formBased.loginPage("/login")
-                            .defaultSuccessUrl("/products").permitAll()
-                            .failureUrl("/login?error=true").permitAll();
+        )
+        .formLogin((formBased) -> {
+            formBased
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/products").permitAll()
+                    .failureUrl("/login?error=true").permitAll();
                 }
-        ).logout(
-                (logout) -> logout
+        ).logout((logout) -> logout
                         .invalidateHttpSession(true)
                         .clearAuthentication(true)
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl("/login?logout")
                         .permitAll()
         ).exceptionHandling(
                 except -> except
